0578a57b19c59692296520f0017d23b25d294eb2
ko <kyokucho1989@gmail.com>
2019-11-22 05:27:06 +0900

    odai4

---
+++ question/q4.rb
@@ -0,0 +1,7 @@
+# Q4. 以下の二つの配列を合体させた新しい配列を作成してください
+# ["dog", "cat", "fish"]
+# ["bird", "bat", "tiger"]
+
+animals_1 =  ["dog", "cat", "fish"]
+animals_2 =  ["bird", "bat", "tiger"]
+puts (animals_1 + animals_2).inspect
