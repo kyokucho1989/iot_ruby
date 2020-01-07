21f4dd7e566348d4ee0619a4a9222dec4a66da25
ko <kyokucho1989@gmail.com>
2020-01-08 04:55:14 +0900

    共同開発課題3の途中まで

--- project/hello_world_sample/config/routes.rb
+++ project/hello_world_sample/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 # 左側がURL 右側がコントローラの名前とアクション
-  get '/homes', to: 'homes#index'  
+  get '/homes', to: 'homes#index'
+  get '/news', to: 'news#addplant' 
 end
